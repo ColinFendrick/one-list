86fc5bc86f9a86689f14c861835f04e4b6850997
Colin Fendrick <colin.fendrick@gmail.com>
2017-03-28 14:43:49 -0400

    Adventure mode completed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  document.getElementById('addButton').addEventListener('click', function (event) {
+    addListElement()
+  })
+
+  document.getElementById('textBox').addEventListener('keyup', function (event) {
+    event.preventDefault()
+    if (event.keyCode === 13) {
+      addListElement()
+    }
+  })
+  const addListElement = () => {
+    let newText = document.getElementById('textBox').value
+    let liAdd = document.createElement('li')
+    liAdd.textContent = newText
+    document.getElementById('list').appendChild(liAdd)
+    liAdd.addEventListener('click', function (event) {
+      if (liAdd.className) {
+        liAdd.className = null
+      } else {
+        liAdd.className = 'completed'
+      }
+    })
+    liAdd.addEventListener('dblclick', function (event) {
+      liAdd.parentNode.removeChild(liAdd)
+    })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
